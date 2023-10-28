028de4c9a50ea0d15fac2a045687d1d793403ac9
Randall543 <silentvoice5001@gmail.com>
2023-10-28 15:30:15 -0400

    fixed floating point issues and general spatial hash

--- shash.py
+++ shash.py
@@ -15,12 +15,16 @@ TIMESTEP = 0.025
 
 boundary_x = 0.5
 boundary_y = 0.5
-max_move = 0.05
-circle_rad = 0.05
+# max_move = 0.05
+max_move = 2
+# circle_rad = 0.05
+circle_rad = 25
 AgentSize = circle_rad*2
 numCircles = 30 
-MAX_FORCE = 0.01
-MAX_SPEED = 0.008
+# MAX_FORCE = 0.01
+# MAX_SPEED = 0.008
+MAX_FORCE = 1
+MAX_SPEED = 0.8
 lastTime = time.time_ns()
 frames = 0
 totalTime = 0
@@ -32,31 +36,19 @@ previousAVG = 0
 fps = 0.0
 fps_avg = 0.0
 
-# Callback for mouse button events
-def mouse_button_callback(window, button, action, mods):
-    x, y = glfw.get_cursor_pos(window)
-    if button == glfw.MOUSE_BUTTON_LEFT and action == glfw.PRESS:
-        print(x,y)
-        print("Left mouse button pressed!")
-    elif button == glfw.MOUSE_BUTTON_LEFT and action == glfw.RELEASE:
-        print("Left mouse button released!")
-    elif button == glfw.MOUSE_BUTTON_RIGHT and action == glfw.PRESS:
-        print("Right mouse button pressed!")
-    elif button == glfw.MOUSE_BUTTON_RIGHT and action == glfw.RELEASE:
-        print("Right mouse button released!")
+# spatial hash
+window_height = 800 # y : rows
+window_width = 800  # x : cols
+divisions = 8 # This controls how many cells the grid will be divided into. division division
+numCells = divisions ** 2
+cell_w = window_width // divisions
+cell_h = window_height // divisions
+grid_w = divisions
+sHash = {k : []  for k in range(numCells)}
 
-# Callback for keyboard events
-def key_callback(window, key, scancode, action, mods):
-    if action == glfw.PRESS:
-        if key == glfw.KEY_ESCAPE:
-            print("Escape key pressed!")
-            glfw.set_window_should_close(window, True)
-        elif key == glfw.KEY_A:
-            print("A key pressed!")
-        elif key == glfw.KEY_B:
-            print("B key pressed!")
-        # ... add more keys as needed
-
+# hash function: use coordinated to get associated cell
+def getGridCell(x,y) -> int:
+    return (x // cell_w) + (y // cell_h) * grid_w
 
 # Initialize the library
 if not glfw.init():
@@ -72,23 +64,28 @@ if not window:
 # Make the window's context current
 glfw.make_context_current(window)
 
-# Set callbacks
-glfw.set_mouse_button_callback(window, mouse_button_callback)
-glfw.set_key_callback(window, key_callback)
-
-
+# initializes the circles
 circles = []
 
 for i in range(numCircles):
     circle = {
-        'x': random.uniform(-boundary_x, boundary_x),
-        'y': random.uniform(-boundary_y, boundary_y),
+        # 'x': random.uniform(-boundary_x, boundary_x),
+        # 'y': random.uniform(-boundary_y, boundary_y),
+        'x': random.randint(0, window_width),
+        'y': random.randint(0, window_height),
         'v_x': random.uniform(-max_move, max_move),
         'v_y': random.uniform(-max_move, max_move),
         'v_x_goal': random.uniform(-max_move, max_move),
         'v_y_goal': random.uniform(-max_move, max_move)
     }
     circles.append(circle)
+
+# added to spatial hash (TODO combine this into initialize stage)
+def update_sHash():
+    for c in circles:
+        x , y = c['x'] , c['y']
+        gridCell = getGridCell(x, y)
+        sHash[gridCell].append(c)
 
 def distanceF(x1, y1, x2, y2):
     return math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
@@ -102,6 +99,7 @@ def update_circles():
         circle['y'] += circle['v_y']
 
         # Apply constraints to stay within the boundaries
+        '''
         if circle['x'] < -0.5:
             circle['x'] = 0.5
         if circle['y'] < -0.5:
@@ -110,6 +108,16 @@ def update_circles():
             circle['x'] = -0.5
         if circle ['y'] > 0.5:
             circle['y'] = -0.5
+        '''
+        # changed boundies to be one more and one less then the set window boundry. Just to make the math work
+        if circle['x'] < 1:
+            circle['x'] = 799
+        if circle['y'] < 1:
+            circle['y'] = 799
+        if circle['x'] > 799:
+            circle['x'] = 1
+        if circle ['y'] > 799:
+            circle['y'] = 1
 
         vx = circle['v_x']
         vy = circle['v_y']
@@ -120,19 +128,25 @@ def update_circles():
         favoidy = 0
         favoidctr = 0
 
-        # Collision checking
-        for j in range(numCircles):
+        circle1x , circle1y = circle['x'], circle['y']
+        gridCell = getGridCell(circle1x, circle1y)
+
+        # skip the cell if less than 2 agents in one cell
+        if len(sHash[gridCell]) < 2:
+            continue
+
+        # looking at all circles within the same cell
+        for curr in sHash[gridCell]:
+            # don't bother checking the same circle against itself
+            if circle is curr:
+                continue
+
             circle1x = circle['x']
             circle1y = circle['y']
 
-            circle2 = circles[j]
+            circle2 = curr
             circle2x = circle2['x']
             circle2y = circle2['y']
-
-            # don't bother checking the same circle against itself
-            if i == j:
-                continue    
-            
   
             distance = distanceF(circle1x, circle1y, circle2x, circle2y)
 
@@ -170,45 +184,59 @@ def update_circles():
             circle['v_x'] = vx
             circle['v_y'] = vy
             circle['x'] += TIMESTEP*vx
-            circle['y'] += TIMESTEP*vy               
+            circle['y'] += TIMESTEP*vy           
 
 
 """
 This is the drawCircle helper function
 """
 def drawCircle(x, y, r, numberOfSegments):
-    glTranslatef(x, y, 0)
     
-    # glColor3f(0, 0, 0, 1)
-    glColor3f(0, 0, 1)
-    glBegin(GL_LINE_LOOP)
-    i = 0
-    while (i < numberOfSegments):
-        angle = 360 * i / numberOfSegments
-        cx = r*math.cos(angle)
-        cy = r*math.sin(angle)
-
-        glVertex2f(x  + cx, y + cy)
-        i += 1
-
-    glEnd()
-    
-    glTranslatef(x, y, 0)
-
-    # glColor3f(0.807, 0, 0, 1) 
-    glColor3f(0.807, 0, 1) 
-    glBegin(GL_TRIANGLE_FAN)
-    i = 0
-    while (i < numberOfSegments):
-        angle = 2 * math.pi * i / numberOfSegments
-        cx = r*math.cos(angle)
-        cy = r*math.sin(angle)
-
-        glVertex2f(cx, cy)
-        i += 1
-
+    glColor(0.0, 0.0, 0.0)
+    glLineWidth(2.0)  # Set the line width
+    glBegin(GL_LINE_LOOP)  # Use GL_LINE_LOOP to draw the boundary
+    for i in range(360):
+        theta = i * (math.pi / 180)
+        glVertex2d(x + r * math.cos(theta), y + r * math.sin(theta))
     glEnd()
 
+    glColor(0.807, 0.0, 0.0)
+    glBegin(GL_POLYGON)
+    for i in range(360):
+        theta = i * (math.pi / 180)
+        glVertex2d(x + r * math.cos(theta),y + r * math.sin(theta))
+    glEnd()
+    # glTranslatef(x, y, 0)
+    
+    # # glColor3f(0, 0, 0, 1)
+    # glColor3f(0, 0, 1)
+    # glBegin(GL_LINE_LOOP)
+    # i = 0
+    # while (i < numberOfSegments):
+    #     angle = 360 * i / numberOfSegments
+    #     cx = r*math.cos(angle)
+    #     cy = r*math.sin(angle)
+
+    #     glVertex2f(x  + cx, y + cy)
+    #     i += 1
+
+    # glEnd()
+    
+    # glTranslatef(x, y, 0)
+
+    # # glColor3f(0.807, 0, 0, 1) 
+    # glColor3f(0.807, 0, 1) 
+    # glBegin(GL_TRIANGLE_FAN)
+    # i = 0
+    # while (i < numberOfSegments):
+    #     angle = 2 * math.pi * i / numberOfSegments
+    #     cx = r*math.cos(angle)
+    #     cy = r*math.sin(angle)
+
+    #     glVertex2f(cx, cy)
+    #     i += 1
+
+    # glEnd()
 
 
 def getFPS():
@@ -228,19 +256,6 @@ def getFPS():
         updateFrames = 0
     return fps_avg, fps
 
-# spatial hash
-window_height = 0.5 # y : rows
-window_width = 0.5  # x : cols
-divisions = 4
-numCells = divisions ** 2
-cell_w = window_width // divisions
-cell_h = window_height // divisions
-grid_w = divisions
-grid = {k : []  for k in range(numCells)}
-
-# hash function: use coordinated to get associated cell
-def getGridCell(x,y):
-    return (x // cell_w) + (y // cell_h) * grid_w
 
 # Main loop
 while not glfw.window_should_close(window):
@@ -251,7 +266,19 @@ while not glfw.window_should_close(window):
     #Draw circles
     time.sleep(TIMESTEP)
  
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0, window_width, 0, window_height, -1, 1)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
     update_circles() 
+    
+    for i in sHash:
+        sHash[i] = []
+    
+    update_sHash()
 
     for circle in circles:
         glPushMatrix()
